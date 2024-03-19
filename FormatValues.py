5c2f6b1328f674f0bcd76b23afec5e4c6fd3ee46
Brian Janes <brian.janes@keyin.com>
2024-03-19 16:51:15 -0230

    initial commit

---
+++ FormatValues.py
@@ -0,0 +1,129 @@
+def FDollar2(DollarValue):
+    # Function will accept a value and format it to $#,###.##.
+
+    DollarValueStr = "${:,.2f}".format(DollarValue)
+
+    return DollarValueStr
+
+
+def FDollar0(DollarValue):
+    # Function will accept a value and format it to $#,###.##.
+
+    DollarValueStr = "${:,.0f}".format(DollarValue)
+
+    return DollarValueStr
+
+
+def FComma2(Value):
+    # Function will accept a value and format it to $#,###.##.
+
+    ValueStr = "{:,.2f}".format(Value)
+
+    return ValueStr
+
+
+def FComma0(Value):
+    # Function will accept a value and format it to $#,###.##.
+
+    ValueStr = "{:,.0f}".format(Value)
+
+    return ValueStr
+
+
+def FNumber0(Value):
+    # Function will accept a value and format it to $#,###.##.
+
+    ValueStr = "{:.0f}".format(Value)
+
+    return ValueStr
+
+
+def FNumber1(Value):
+    # Function will accept a value and format it to $#,###.##.
+
+    ValueStr = "{:.1f}".format(Value)
+
+    return ValueStr
+
+
+def FNumber2(Value):
+    # Function will accept a value and format it to $#,###.##.
+
+    ValueStr = "{:.2f}".format(Value)
+
+    return ValueStr
+
+
+def FDateS(DateValue):
+    # Function will accept a value and format it to yyyy-mm-dd.
+
+    DateValueStr = DateValue.strftime("%Y-%m-%d")
+
+    return DateValueStr
+
+
+def FDateM(DateValue):
+    # Function will accept a value and format it to dd-Mon-yy.
+
+    DateValueStr = DateValue.strftime("%d-%b-%y")
+
+    return DateValueStr
+
+
+def FDateL(DateValue):
+    # Function will accept a value and format it to Day, Month dd, yyyy.
+
+    DateValueStr = DateValue.strftime("%A, %B %d, %Y")
+
+    return DateValueStr
+
+# Brian'a Additions to this file.
+
+
+def format_postal_code(postal_code):
+    """
+    Accepts a value that has already been validated through the check_post_code function and format it to X#X #X#.
+
+    Retuns a formatted postal code.
+    """
+    postal_code = postal_code.upper()
+    formatted_postal_code = postal_code[0:3] + " " + postal_code[3:6]
+    return formatted_postal_code
+
+
+def check_postal_code(postal_code):
+    """
+    Accepts a value and checks if it is a valid postal code in terms of character count, alphabetical characters, and numerical characters. 
+
+    Returns a formatted postal code or an error message.
+    """
+    if len(postal_code) != 6:
+        return "Data Entry Error (Invalid postal code) - Character count issue. Please try again."
+    elif postal_code[0].isalpha() == False or postal_code[2].isalpha() == False or postal_code[4].isalpha() == False:
+        return "Data Entry Error (Invalid postal code) - Alphabetical character issue. Please try again."
+    elif postal_code[1].isdigit() == False or postal_code[3].isdigit() == False or postal_code[5].isdigit() == False:
+        return "Data Entry Error (Invalid postal code) - Numerical character issue. Please try again."
+    else:
+        return format_postal_code(postal_code)
+
+def format_phone_num(phone_num):
+    """
+    Accepts a value that has already been validated through the check_phone_num function and format it to (###) ###-####.
+
+    Returns a formatted phone number.
+    """
+    formatted_phone_num = "(" + phone_num[0:3] + ") " + phone_num[3:6] + "-" + phone_num[6:10]
+    return formatted_phone_num
+
+def check_phone_num(phone_num):
+    """
+    Accepts a value and checks if it is a valid phone number in terms of character count and numerical characters.
+
+    Returns a formatted phone number or an error message.
+    """
+    if len(phone_num) != 10:
+        return "Data Entry Error (Invalid phone number) - Character count issue. Please try again."
+    elif phone_num.isdigit() == False:
+        return "Data Entry Error (Invalid phone number) - Phone number must only be numerical values. Please try again."
+    else:
+        return format_phone_num(phone_num)
---
+++ mtg-tracker.py
@@ -0,0 +1,118 @@
+# DESCRIPTION: A Magic the gathering tournment results tracker
+# AUTHOR: Brian Janes   
+# DATE: March 19th, 2024
+
+# Import the necessary libraries
+import math
+import sys
+import datetime
+import FormatValues as FV
+import os
+import time
+
+# Define the main function
+print()
+print("  M:TG Tournament Statistics Tracker")
+print()
+print()
+while True:
+    number_of_rounds = int(input("Enter the number of rounds: "))
+    if number_of_rounds < 3:
+        print("Please enter a number greater than 3")
+    else:
+        break
+print()
+print("There are a lot of formats in Magic the Gathering.  Here are a few examples:")
+print("Standard, Modern, Legacy, Vintage, Pioneer, Commander, Brawl, Pauper, and more.")
+tournament_format = input("Enter the format of the tournament:  ").title()
+
+print()
+print("There are a lot of archetypes in Magic the Gathering.  Here are a few examples:")
+print("Midrange, Control, Aggro, Combo, Tempo, Ramp, and more.")
+user_deck_archetype = input("Enter the archetype of your deck:  ").title()
+# Create a list to store the results of each round
+round_details = []
+
+# Iterating over the 
+for round in range(number_of_rounds):
+    os.system('clear')
+    print()
+    print(f"Entering details for round {round + 1}:")
+    print()
+    # Get details for this round
+    while True:
+        play_draw = input("Did you play or draw this round? (P/D): ")
+        if play_draw.upper() not in ["P", "D"]:
+            print("Please enter P or D.")
+        else:
+            break
+    matchup = input("What was the matchup this round?: ")
+
+    # Get details for this round
+    while True:
+        result = input("Did you win, lose, or draw this match? (W/L/D): ")
+        if result.upper() not in ["W", "L", "D"]:
+            print("Please enter W, L, or D.")
+        else:
+            break
+
+    # Initializing these variables to 0
+    wins = 0
+    losses = 0
+    draws = 0
+
+    if result.upper() == "W":
+        wins = 2
+        losses = int(input("How many games did you lose this round? (0 or 1): "))
+    elif result.upper() == "L":
+        losses = 2
+        wins = int(input("How many games did you win this round? (0 or 1): "))
+    elif result.upper() == "D":  
+        wins = int(input("How many games did you win this round? (0 or 1): "))
+        if wins == 1:
+            losses = 1
+            draws = 1
+        else:
+            losses = int(input("How many games did you lose this round? (0 or 1): "))
+            draws = 1 if losses == 0 else 0
+
+    # Initialize mulligan counters
+    player_mulligans = 0
+    opponent_mulligans = 0
+
+    # Calculate total games
+    total_games = wins + losses + draws
+
+    # Ask for mulligan information for each game
+    for game in range(total_games):
+        print()
+        player_mulligans += int(input(f"How many times did you mulligan in game {game + 1}?: "))
+        opponent_mulligans += int(input(f"How many times did your opponent mulligan in game {game + 1}?: "))
+
+    print()
+    notes = input("Any notes for this round?: ")
+
+print()
+for _ in range(5):  # Change to control number of 'blinks'
+    print('Preparing for next round...', end='\r')
+    time.sleep(.3)  # To create the blinking effect
+    sys.stdout.write('\033[2K\r')  # Clears the entire line and carriage returns
+    time.sleep(.3)
+# Clear the console at the end of each round
+os.system('clear')
+
+# Store details in a dictionary
+round_details = {
+    "play_draw": play_draw,
+    "player mulligans": player_mulligans,
+    "opponent mulligans": opponent_mulligans,
+    "matchup": matchup,
+    "wins": wins,
+    "losses": losses,
+    "draws": draws,
+    "notes": notes
+}
+# Add the details for this round to the list
+round_details.append(round_details)
+
+print(round_details)
